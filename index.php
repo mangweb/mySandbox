2abd68e3a0eea734208d0b3de4d66e09556a462b
mangweb <anagnam@yahoo.com>
2014-06-13 21:26:25 +0300

    modified to add boilerplate html markup

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title></title>
+		<link rel='stylesheet' href='style.css'>
+	</head>
+	
+	<body>
+	
+	
+		<script src=''></script>
+		<script src=''></script>
+	</body>
+</html>
